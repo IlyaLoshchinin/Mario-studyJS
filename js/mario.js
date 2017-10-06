7dc3d212a72182d1053a3688d9e4a859f3a11a2f
ILLIA <illia.loshchinin@gmail.com>
2017-10-06 19:40:46 +0200

    implement a whole task

--- js/mario.js
+++ js/mario.js
@@ -1,50 +1,86 @@
-/*
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
+document.addEventListener('DOMContentLoaded', function() {
+    /*
+     * printPyramid
+     *
+     * Prints to the console a pyramid of '#' characters of the specified height
+     * For example, if height is 5, the console will look like this:
+     *          ##
+     *         ###
+     *        ####
+     *       #####
+     *      ######
+     */
 
-const button = document.querySelector("button");
-button.addEventListener("click", printPyramid);
 
-function printPyramid() {
-    const count = document.querySelector("input");
-    if (count.value.length == 0) {
-        return;
+
+    var slider = document.querySelector("input[type='range']");
+    var brick_select = document.querySelector("select")
+    var counter = document.getElementById("size-upstairs");
+    var pyramid = document.getElementById("pyramid");
+
+    slider.addEventListener("change", printPyramid);
+    brick_select.addEventListener("change", printPyramid);
+
+
+
+    if (matchMedia) {
+        const mq = window.matchMedia("(max-width: 575px)");
+        mq.addListener(WidthChange);
+        WidthChange(mq);
     }
-    const construction = document.getElementById("construction");
-    if (construction) {
-        document.getElementById("construction").remove();
-    };
 
-    const height = parseInt(count.value, 10);
-    const pyramid = document.getElementById("pyramid");
-    const block = document.createElement("span");
-    const emptyBlock = block.cloneNode();
 
-    block.className = "block";
-    emptyBlock.className = "empty-block";
+    function WidthChange(mq) {
+        if (mq.matches) {
+            slider.max = 12;
+            slider.value = 6;
+            printPyramid();
+        } else {
+            slider.max = 20;
+            slider.value = 10;
+            printPyramid();
+        }
 
-    let wrapper = document.getElementsByClassName("wrapper")[0];
-    if (wrapper) {
-        wrapper.remove();
     }
-    wrapper = document.createElement("div");
-    wrapper.className = "wrapper";
-    pyramid.appendChild(wrapper);
 
 
-    let space = height - 1;
-    for (let i = 2; i < height + 2; i++) {
-        let div = document.createElement("div");
-        div.innerHTML = emptyBlock.outerHTML.repeat(space--) + block.outerHTML.repeat(i);
-        wrapper.appendChild(div);
+
+    printPyramid();
+
+    function printPyramid() {
+
+
+        counter.textContent = slider.value;
+
+
+        const height = slider.valueAsNumber;
+        const block = document.createElement("span");
+        const emptyBlock = block.cloneNode();
+
+        var brick = brick_select.options[brick_select.selectedIndex].value;
+
+        if (brick != "square") {
+            block.className = "block symbol";
+            block.innerHTML = brick;
+        } else {
+            block.className = "block fill";
+        }
+        emptyBlock.className = "empty-block";
+
+        if (pyramid.hasChildNodes()) {
+            pyramid.innerHTML = '';
+        }
+
+        let space = height - 1;
+        for (let i = 2; i < height + 2; i++) {
+            let div = document.createElement("div");
+            div.innerHTML = emptyBlock.outerHTML.repeat(space--) + block.outerHTML.repeat(i);
+            pyramid.appendChild(div);
+        }
+
     }
-    count.value = "";
-}
+
+
+
+
+});
